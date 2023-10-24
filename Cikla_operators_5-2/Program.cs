02386b307a94a61d9cbe8a14f6870caa51b87927
Grinchs <marcisg2345@gmail.com>
2023-10-24 08:57:36 +0300

    Add project files.

---
+++ Cikla_operators_5-2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ievadiet sākotnējo summu: ");
+        double saktotnejaSumma = double.Parse(Console.ReadLine());
+
+        Console.Write("Ievadiet noguldījuma laiku (gadi): ");
+        int laiks = int.Parse(Console.ReadLine());
+
+        Console.Write("Ievadiet procentu likmi (gada procentos): ");
+        double procentuLikme = double.Parse(Console.ReadLine());
+
+        for (int gads = 1; gads <= laiks; gads++)
+        {
+            saktotnejaSumma += saktotnejaSumma * (procentuLikme / 100);
+            Console.WriteLine($"Pēc {gads}. gada: {saktotnejaSumma:C}");
+        }
+
+        Console.WriteLine($"Noguldījuma beigu summa pēc {laiks} gadiem ir {saktotnejaSumma:C}");
+    }
+}
